71c86d1f28f4b4d2154b6c767afe8177879b0238
raine410 <66644254+raine410@users.noreply.github.com>
2020-06-08 15:42:33 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NzE3NTY0NzkzODIwODA3MjI5.XtcKkA.PL8ilB5_74p5VOq7dQ1LMZRNhxU);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
