105bffdab18e395eb278f31544caa30a0fe684fe
kurohinotori <kurohinotori@gmail.com>
2020-06-09 21:06:55 -0500

    Fix whitespace and typo

--- bot.js
+++ bot.js
@@ -1,48 +1,31 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-    client.channels.get("361298665689710592").send("I AM ALIVE")
-    
-
+    client.channels.get("361298665689710592").send("I AM ALIVE");
 });
 
 //set the prefix
- const prefix = "!"
- client.on("message", (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;    //exits and stops if no prefix or if the message is a bot
-    if (message.content.startsWith(prefix + "poke")){
-         message.reply("You poked the oddish and he is now saw.")
-     } 
-     
- })
+const prefix = "!"
+client.on("message", (message) => {
+    //exits and stops if no prefix or if the message is a bot
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
- 
+    if (message.content.startsWith(prefix + "poke")) {
+        message.reply("You poked the oddish and he is now sad.");
+    }
+})
 
 client.on('message', message => {
-
     if (message.content === 'pong') {
+        message.reply('ping');
+    }
 
-       message.reply('ping');
-
-       }
-  if (message.content === 'hello pokemonbot') {
-
-       message.reply('hello');}
-
-
+    if (message.content === 'hello pokemonbot') {
+        message.reply('hello');
+    }
 });
 
-
-
-
- 
-
 // THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
