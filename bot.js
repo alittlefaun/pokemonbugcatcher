0520855c72dcffe891e070c18f151272f2f01c17
raine410 <alittlefaun@gmail.com>
2020-06-11 16:24:57 -0500

    changed from get to fetchs

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ const client = new Discord.Client();
  */
 client.on('ready', () => {
     console.log('I am ready!');
-    client.channels.get("361298665689710592").send("I AM ALIVE");
+    client.channels.fetch("361298665689710592").send("I AM ALIVE");
 });
 
 //set the prefix
