8827f3c8ff1d5886b0c338ce29223484b5e48334
raine410 <alittlefaun@gmail.com>
2020-06-11 21:26:19 -0500

    added bugcatchers.json file

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+//takes the .env package and calls the function config on it which loads all the environment variables; REMOVE FROM UPLOADED BUILD
+//require('dotenv').config();  //REMOVEREMOVEREVMOE
 const Discord = require('discord.js');
 //extracts the required classes from the discord.js module
 const { Client, MessageAttachment } = require('discord.js');
@@ -22,6 +24,7 @@ client.on('ready', () => {
 //set the prefix
 const prefix = "!"
 client.on("message", (message) => {
+        
     if (message.content.toLowerCase() === 'hello') {
         message.channel.send(`Hello bug catcher ${message.author}!`);
     }
@@ -31,18 +34,22 @@ client.on("message", (message) => {
 
     //brings up menu
     if (message.content.startsWith(prefix + "menu")) {
-        message.reply("These are my menu commands. \n !menu - brings up my menu. \n !start - starts your bug catching adventure. \n !incubator - checks on the status of your egg incubator \n !team - lists out your six active pokemon" );
+        message.reply("These are my menu commands.\n" +
+            "!menu - brings up my menu.\n" +
+            "!start - starts your bug catching adventure.\n" +
+            "!incubator - checks on the status of your egg incubator\n" +
+            "!team - lists out your six active pokemon"); 
     }
     //!start
     if (message.content.startsWith(prefix + "start")) {
         message.reply("Here is an egg for you.");
         const attachment = new MessageAttachment('https://i.imgur.com/WRCr8c3.png');
         message.channel.send(attachment);
-        client.bugcatchers [message.author.username] = {
-            message: message.content
+        client.bugcatchers [message.author.id] = {
+            "trainer name": message.author.username
         }
         fs.writeFile("./bugcatchers.json", JSON.stringify (client.bugcatchers, null, 4), err => {
-            .catch(err => console.log(err));
+            if (err) console.log(err);
             message.channel.send ("We are now documenting your adventure.");
         });
     }
