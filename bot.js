530be39ff92ab958b60c0d34982e41aa047a743d
kurohinotori <kurohinotori@gmail.com>
2020-06-16 17:15:22 -0500

    Update command structure, add jokes, and migrate poke Migrating unmerged poke functionality Added 21 unique League of Legends jokes Updated command structure to be scalable

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 //takes the .env package and calls the function config on it which loads all the environment variables; REMOVE FROM UPLOADED BUILD
-require('dotenv').config();  //REMOVEREMOVEREMOVE
+require('dotenv').config();
 const Discord = require('discord.js');
 //extracts the required classes from the discord.js module
 const { Client, MessageAttachment } = require('discord.js');
@@ -19,8 +19,88 @@ for (i = 0; i < Object.keys(availablePokemon).length; i++) {
     }
 }
 
+function commandJoke(message) {
+    const arrayOfKeysInJokes = Object.keys(jokes);
+    //returns a random key between 1 (inclusive) and the # of keys (inclusive) in jokes.json
+    const randomKeyForJokes = Math.floor(Math.random() * arrayOfKeysInJokes.length);
+    console.log(randomKeyForJokes);
+    //returns the value associated with the randomly generated key for jokes.json
+    const jokeMsg = jokes[arrayOfKeysInJokes[randomKeyForJokes]];
+    message.channel.send(jokeMsg);
+}
+
+/**
+ * 'poke' Command function. Stores all poke events, removing them after a minute has
+ * passed. Replies to user with caretaker's reaction based on number of stored pokes.
+ */
+let pokeTimestamps = [];
+function commandPoke(message) {
+    let timestamp = Date.now();
+    const minInMilli = 60000; // minutes in milliseconds
+    const madThreshold = 2;
+    const rageThreshold = 4;
+
+    pokeTimestamps.filter(ts => ts < timestamp - minInMilli);
+    pokeTimestamps.push(timestamp);
+
+    let pokesPerMinute = pokeTimestamps.length;
+    let oddishStatus = " is now sad.";
+    let attachmentUrl = 'https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/9/90/Oddish_sad.jpg';
+
+    if (pokesPerMinute > madThreshold) {
+        oddishStatus = "getting angry...";
+        attachmentUrl = 'https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/6/63/Screenshot_2019-02-26_oddish_-_Google_Search.png';
+    }
+
+    if (pokesPerMinute > rageThreshold) {
+        oddishStatus = "enraged! Watch out!"
+        attachmentUrl = 'https://vignette.wikia.nocookie.net/legendsofthemultiuniverse/images/f/f5/609355abc432f86446a7b615b28e2229cabac0f6_hq.gif';
+    }
+
+    const attachment = new Discord.MessageAttachment(attachmentUrl);
+    message.reply(`you poked the oddish, and he is ${oddishStatus}`, attachment);
+}
+
+//set the prefix
+const prefix = "!"
+let commands = {
+    "joke": {
+        "help": "tells a random League of Legends joke",
+        "command": commandJoke
+    },
+    "menu": {
+        "help": "brings up my menu",
+        "command": commandMenu
+    },
+    "poke": {
+        "help": "poke the oddish for a reaction",
+        "command": commandPoke
+    },
+    "start": {
+        "help": "take up responsibility as an egg nurturer",
+        "command": commandMenu,
+    },
+    "test": {
+        "help": "test command",
+        "command": commandMenu
+    }
+}
+
+/**
+ * 'menu' Command function. Prints all commands in `commands` object
+ * with their help message.
+ */
+function commandMenu(message) {
+    let msg = "These are my menu commands:\n";
+    for (const cmd in commands) { //cmd is short for command
+        msg += `${prefix}${cmd} - ${commands[cmd].help}\n`;
+    }
+    message.channel.send(msg);
+}
+
 function hatch(message, isShiny) {
     //returns a random index number for the array of availableHatchablePokemon
+    // TODO: @raine410, Make a function for getting a random index in an array.
     const randomIndex = Math.floor(Math.random() * availableHatchablePokemon.length);
     //retuns the number of the pokemon randomly chosen from availableHatchablePokemon, this will be used as a KEY
     const randomPokemonNumber = availableHatchablePokemon[randomIndex];
@@ -40,11 +120,7 @@ function hatch(message, isShiny) {
     const attachedImage = new MessageAttachment(url);
     message.reply(msg, attachedImage)
         .catch((rejectionReason) => { console.log(rejectionReason) });
-
 };
-
-
-
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -57,25 +133,20 @@ client.on('ready', () => {
         .catch(console.error);
 });
 
-//set the prefix
-const prefix = "!"
 client.on("message", (message) => {
-
     if (message.content.toLowerCase() === 'hello') {
         message.channel.send(`Hello ${message.author}! Please type <!menu> if you would be interested in caring for Pokemon eggs.`);
     }
 
     //exits and stops if no prefix or if the message is a bot
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    //brings up menu
-    if (message.content.startsWith(prefix + "menu")) {
-        message.reply("These are my menu commands.\n" +
-            "!menu - brings up my menu.\n" +
-            "!start - take up responsibility as an egg nurturer.\n" +
-            "!incubator - checks on the status of your egg incubator\n" +
-            "!other - other command???");
+    // assume all commands are just prefix + command
+    const cmd = message.content.slice(1); // get rid of the prefix.
+    if (cmd in commands) {
+        commands[cmd].command(message);
     }
+    // TODO: @raine410, please implement these functions in the new command structure... plz?
+    /*
     //!start
     if (message.content.startsWith(prefix + "start")) {
         const shinyFactor = Math.random();
@@ -102,21 +173,6 @@ client.on("message", (message) => {
             }, 10000);
         }
     }
-
-    //!poke
-    if (message.content.startsWith(prefix + "poke")) {
-        message.reply("You poked the oddish and he is now sad.");
-    }
-    //!joke
-    if (message.content.startsWith(prefix + "joke")) {
-        const arrayOfKeysInJokes = Object.keys(jokes);
-        //returns a random key between 1 (inclusive) and the # of keys (inclusive) in jokes.json
-        const randomKeyForJokes = Math.floor(Math.random() * arrayOfKeysInJokes.length) + 1;
-        console.log(randomKeyForJokes);
-        //returns the value associated with the randomly generated key for jokes.json
-        const jokeMsg = jokes[randomKeyForJokes];
-        message.reply(`\n ${jokeMsg}`);
-    }
     //!test
     if (message.content.startsWith(prefix + "test")) {
         message.reply(availablePokemon);
@@ -124,7 +180,8 @@ client.on("message", (message) => {
         //returns # of keys (different Pokemon) available in availablePokemon.json
         console.log(Object.keys(availablePokemon).length);
     }
+    */
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
